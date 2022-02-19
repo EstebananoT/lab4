4b6ffd613541f976c4b70093d86e8345ef104397
Estebanano T <mordekay101@gmail.com>
2022-02-18 23:58:33 -0500

    implementación del modelo

--- src/test/java/hangman/model/GameScoreTest.java
+++ src/test/java/hangman/model/GameScoreTest.java
@@ -13,20 +13,48 @@ public class GameScoreTest {
      * 2. Valores Incorrectos: Valores negativos para cualquier parámetro
      * 3. Valores :Retornos menores a 0
      */
-    private OriginalScore score;
+    private OriginalScore scoreO;
     @Test
     public void originalScoreTest() {
-        score = new OriginalScore();
+        scoreO = new OriginalScore();
         int numero = 0;
         try {
-            numero = score.calculateScore(10, 1);
+            numero = scoreO.calculateScore(10, 1);
             Assert.assertEquals(90, numero);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private BonusScore scoreB;
     @Test
     public void calculateScoreBonus(){
+        scoreB = new BonusScore();
+        int numero = 0;
+        try {
+            numero = scoreB.calculateScore(1, 10);
+            Assert.assertEquals(0, numero);
 
+            numero = scoreB.calculateScore(100, 10);
+            Assert.assertEquals(950, numero);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private PowerScore scoreP;
+    @Test
+    public void calculateScorePower(){
+        scoreP = new PowerScore();
+        int numero = 0;
+        try {
+            numero = scoreP.calculateScore(1, 10);
+            Assert.assertEquals(0, numero);
+
+            numero = scoreP.calculateScore(100, 10);
+            Assert.assertEquals(500, numero);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
